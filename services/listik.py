207cc8e565a38f68a4385a78da1cd8026705ed7b
WATF-F <koltakovmatvej@gmail.com>
2025-04-03 23:42:35 +0300

    initial commit

---
+++ services/listik.py
@@ -0,0 +1,5 @@
+data_demo ={"key":"key_demo", "sicret_key":"secret_key_demo","demo":True}
+data_no_demo = {"key":"key_real", "sicret_key":"secret_key_real","demo":False}
+acsses = {"key":data_no_demo["key"], 
+          "sicret_key": data_no_demo["sicret_key"],
+          "demo":data_no_demo['demo']}
--- telegram-no-api/services/listik.py
+++
@@ -1,5 +0,0 @@
-data_demo ={"key":"your_key_demo", "sicret_key":"your_secret_key_demo","demo":True}
-data_no_demo = {"key":"your_key_real", "sicret_key":"your_secret_key_real","demo":False}
-acsses = {"key":data_no_demo["key"], 
-          "sicret_key": data_no_demo["sicret_key"],
-          "demo":data_no_demo['demo']}
